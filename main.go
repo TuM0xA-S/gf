d5b04a2bc05e6207214ca7436b6f459915bce56e
TuM0xA <tum0xa-00@mail.ru>
2021-05-17 20:11:39 +0300

    basic functionality

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/fs"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"regexp"
 	"runtime"
 	"sync"
 )
+
+type bfsCallback func(path string, info os.FileInfo) error
 
 func stringArrAsChan(arr []string, ch chan string) {
 	go func() {
@@ -26,19 +31,31 @@ func stringChanAsArr(ch chan string, res *[]string, signal chan bool) {
 	signal <- true
 }
 
-func bfsWorker(input chan string, output chan string, action func(path string, info os.FileInfo), wg *sync.WaitGroup) {
+func bfsWorker(input chan string, output chan string, action bfsCallback, wg *sync.WaitGroup) {
 	for path := range input {
 		info, err := os.Lstat(path)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 			continue
 		}
-		action(path, info)
+
+		if action(path, info) == filepath.SkipDir {
+			continue
+		}
+
+		if info.Mode().Type() == fs.ModeSymlink {
+			var err error
+			info, err = os.Stat(path)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+		}
 
 		if info.IsDir() {
 			files, err := ioutil.ReadDir(path)
 			if err != nil {
-				fmt.Println(err)
+				log.Println(err)
 				continue
 			}
 			for _, file := range files {
@@ -49,8 +66,8 @@ func bfsWorker(input chan string, output chan string, action func(path string, i
 	wg.Done()
 }
 
-func bfs(root string, action func(path string, info os.FileInfo)) {
-	curLvlData := []string{root}
+func bfs(roots []string, action bfsCallback) {
+	curLvlData := roots
 	threadCount := 2 * runtime.NumCPU()
 	for len(curLvlData) > 0 {
 		var wgWorkers sync.WaitGroup
@@ -72,20 +89,54 @@ func bfs(root string, action func(path string, info os.FileInfo)) {
 }
 
 func main() {
-	args := os.Args[1:]
-	if len(args) == 0 {
-		fmt.Println("usage: gf <regex of filename> [<directory>]")
-		return
+	log.SetFlags(0)
+	log.SetPrefix("ERROR: ")
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		fmt.Fprintln(flag.CommandLine.Output(), "gf [<options>...] <pattern> [<directores>...]")
+		flag.PrintDefaults()
 	}
+
+	fullPath := flag.Bool("f", false, "show full path")
+	followSymlinks := flag.Bool("s", false, "follow symlinks")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
 	regex, err := regexp.Compile(args[0])
 	if err != nil {
-		fmt.Printf("when compiling regex: %v", err)
+		log.Println(err)
+		os.Exit(2)
+	}
+
+	directories := []string{"./"}
+	if len(args) > 1 {
+		directories = args[1:]
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
 		return
 	}
-	bfs("./", func(path string, info os.FileInfo) {
+
+	bfs(directories, func(path string, info os.FileInfo) error {
 		name := filepath.Base(path)
+		if *fullPath {
+			path = filepath.Join(cwd, path)
+		}
 		if regex.MatchString(name) {
 			fmt.Println(path)
 		}
+		if info.Mode().Type() == fs.ModeSymlink && !*followSymlinks {
+			return filepath.SkipDir
+		}
+
+		return nil
 	})
 }
